8cfb832b9aae59dea5d63d63bb93c206b3774376
Hasankhan170 <hasankhanali0316@gmail.com>
2024-10-23 21:20:29 +0500

    first commit

---
+++ app/index.tsx
@@ -0,0 +1,68 @@
+import { useState, useEffect } from 'react';
+import { Platform, Text, View, StyleSheet } from 'react-native';
+import MapView, { Marker } from 'react-native-maps';
+
+import * as Location from 'expo-location';
+
+export default function App() {
+  const [location, setLocation] = useState<null | any>(null);
+  const [errorMsg, setErrorMsg] = useState<null | string>(null);
+
+  useEffect(() => {
+    (async () => {
+      
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        setErrorMsg('Permission to access location was denied');
+        return;
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+      setLocation(location);
+    })();
+  }, []);
+
+  let text = 'Waiting..';
+  if (errorMsg) {
+    text = errorMsg;
+  } else if (location) {
+    text = JSON.stringify(location);
+  }
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.paragraph}>{text}</Text>
+      {
+        location && <MapView style={styles.map} initialRegion={{
+          latitude: location.coords.latitude,
+          longitude: location.coords.longitude,
+          latitudeDelta: 0.0001,
+          longitudeDelta: 0.0001,
+        }}>
+          <Marker coordinate={{
+            latitude: location.coords.latitude,
+            longitude: location.coords.longitude,
+          }}/>
+        </MapView>
+      }
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 20,
+  },
+  paragraph: {
+    fontSize: 18,
+    textAlign: 'center',
+  },
+  map:{
+    height: '100%',
+    width: '100%',
+  }
+  
+});
